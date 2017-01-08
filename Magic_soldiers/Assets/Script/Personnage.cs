14517a958846ab712753db92e5b8555ac656571d
Thomas <jediknight7962@yaho.fr>
2017-01-08 13:20:31 +0100

    Deplacement sur x z
    Uniquement deplacement clavier

---
+++ Magic_soldiers/Assets/Script/Personnage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personnage : MonoBehaviour {
+
+    #region Attributes
+
+    private int personnageSpeed = 5;
+    private Vector3 directionMove = Vector3.zero;
+    private CharacterController player;
+
+    #endregion
+
+
+
+    #region Unity methods
+    // Use this for initialization
+    void Start () {
+
+        player = GetComponent<CharacterController>();
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Moves();
+
+	}
+    #endregion
+
+
+
+
+
+    #region otherMethods
+
+
+    private void Moves()
+    {
+        directionMove.z = Input.GetAxis("Vertical"); //avant / arriere
+        directionMove.x = Input.GetAxis("Horizontal"); //gauche / droite
+        directionMove = transform.TransformDirection(directionMove * personnageSpeed * Time.deltaTime);
+        player.Move(directionMove);
+
+        
+    }
+
+
+
+    #endregion
+
+
+}
