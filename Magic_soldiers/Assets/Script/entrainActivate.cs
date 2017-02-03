3ae8a7e8e6706d77153a924648de5002e19e42b5
Thomas <jediknight7962@yaho.fr>
2017-02-03 22:58:10 +0100

    Zone d'entrainement
    Commence

---
+++ Magic_soldiers/Assets/Script/entrainActivate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class entrainActivate : MonoBehaviour {
+
+    public static bool IsActivated = true;
+
+    void Start () {
+    }
+	
+	void Update () {
+		
+	}
+
+}
