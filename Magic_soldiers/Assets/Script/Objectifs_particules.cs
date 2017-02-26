f5ac9cc5184e0dc09c004369546d1ca89765fa59
Thomas <jediknight7962@yaho.fr>
2017-02-26 16:25:48 +0100

    Ajout
    Ajout de Secondaires +
    Ajout de faisceau lumineux pour les objectifs

---
+++ Magic_soldiers/Assets/Script/Objectifs_particules.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectifs_particules : MonoBehaviour {
+
+    private ParticleSystem particules;
+	
+	void Start () {
+
+        particules = GetComponent<ParticleSystem>();
+	}
+	
+	
+	void Update () {
+
+        if (particules.name == "Obj_particules" + Canvas_UI.compteur.ToString())
+            particules.Play();
+        else
+            particules.Stop();
+
+	}
+}
