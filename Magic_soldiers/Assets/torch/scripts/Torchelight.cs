cb3d3097b2d94185e1143ae0f92f7538d65b05ad
Ryoma17-Keith <keith.aroul@epita.fr>
2017-04-11 15:20:35 +0200

    Level1 scheme
    ya plus qu'a add les trucs qui font le jeu

--- Magic_soldiers/Assets/torch/scripts/Torchelight.cs
+++ Magic_soldiers/Assets/torch/scripts/Torchelight.cs
@@ -8,32 +8,38 @@ public class Torchelight : MonoBehaviour {
 	public GameObject BaseFlame;
 	public GameObject Etincelles;
 	public GameObject Fumee;
-	public float MaxLightIntensity;
-	public float IntensityLight;
+    public float MaxLightIntensity;
+    public float IntensityLight;
 	
 
-	void Start () {
-        TorchLight.GetComponent<Light>().color = new Color(255f, 150f, 0f, 255f);
+	void Start ()
+    {
+        float r = 255f;
+        float g = 150f;
+        float b = 0f;
+        float a = 255f;
 
-		TorchLight.GetComponent<Light>().intensity=IntensityLight;
+        TorchLight.GetComponent<Light>().color = new Color(r, g, b, a);
+        TorchLight.GetComponent<Light>().range = 200;
+        TorchLight.GetComponent<Light>().intensity = IntensityLight;
 
-		MainFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*20f;
-		BaseFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*15f;	
-		Etincelles.GetComponent<ParticleSystem>().emissionRate=IntensityLight*7f;
-		Fumee.GetComponent<ParticleSystem>().emissionRate=IntensityLight*12f;
+        MainFlame.GetComponent<ParticleSystem>().emissionRate = IntensityLight * 20f;
+        BaseFlame.GetComponent<ParticleSystem>().emissionRate = IntensityLight * 15f;
+        Etincelles.GetComponent<ParticleSystem>().emissionRate = IntensityLight * 7f;
+        Fumee.GetComponent<ParticleSystem>().emissionRate = IntensityLight * 12f;
 	}
 	
 
-	void Update () {
-		
-		TorchLight.GetComponent<Light>().intensity=Mathf.Lerp(4.5f,5.5f,Mathf.Cos(Time.time*30));
+	void Update ()
+    {
+        TorchLight.GetComponent<Light>().intensity = Mathf.Lerp(0.005f, 0.0075f, Mathf.Cos(Time.time * 10));
+        TorchLight.GetComponent<Light>().range = 200;
+        TorchLight.GetComponent<Light>().color = new Color(255f, 150f, 0f, 255f);
 
-		TorchLight.GetComponent<Light>().color= new Color(255f, 150f, 0f, 255f);
-
-		MainFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*20f;
-		BaseFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*15f;
-		Etincelles.GetComponent<ParticleSystem>().emissionRate=IntensityLight*7f;
-		Fumee.GetComponent<ParticleSystem>().emissionRate=IntensityLight*12f;
+        MainFlame.GetComponent<ParticleSystem>().emissionRate = 50 * 20f;
+        BaseFlame.GetComponent<ParticleSystem>().emissionRate = 50 * 15f;
+        Etincelles.GetComponent<ParticleSystem>().emissionRate = 50 * 7f;
+        Fumee.GetComponent<ParticleSystem>().emissionRate = 50 * 12f;
 
 	}
 }
