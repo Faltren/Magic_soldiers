d51469166fec92d9f6169210a8e939753e53e9af
Ryoma17-Keith <keith.aroul@epita.fr>
2017-06-05 20:32:15 +0200

    Lv3
    Lv3

--- Magic_soldiers/Assets/torch/scripts/Torchelight.cs
+++ Magic_soldiers/Assets/torch/scripts/Torchelight.cs
@@ -20,7 +20,7 @@ public class Torchelight : MonoBehaviour {
         float a = 255f;
 
         TorchLight.GetComponent<Light>().color = new Color(r, g, b, a);
-        TorchLight.GetComponent<Light>().range = 200;
+        TorchLight.GetComponent<Light>().range = 10;
         TorchLight.GetComponent<Light>().intensity = IntensityLight;
 
         MainFlame.GetComponent<ParticleSystem>().emissionRate = IntensityLight * 20f;
@@ -33,7 +33,7 @@ public class Torchelight : MonoBehaviour {
 	void Update ()
     {
         TorchLight.GetComponent<Light>().intensity = Mathf.Lerp(0.005f, 0.0075f, Mathf.Cos(Time.time * 10));
-        TorchLight.GetComponent<Light>().range = 200;
+        TorchLight.GetComponent<Light>().range = 10;
         TorchLight.GetComponent<Light>().color = new Color(255f, 150f, 0f, 255f);
 
         MainFlame.GetComponent<ParticleSystem>().emissionRate = 50 * 20f;
